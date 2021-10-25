e044438fc98aca6af678891cb1e6764e26673bdf
Ferry Gilang Ferdiana <ferrygilangf@gmail.com>
2021-10-25 18:51:34 +0700

    Testing

--- app/Http/Controllers/API/ProductCategoryController.php
+++ app/Http/Controllers/API/ProductCategoryController.php
@@ -2,65 +2,48 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\ProductCategory;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 
-class ProductController extends Controller
+class ProductCategoryController extends Controller
 {
     public function all(Request $request)
     {
         $id = $request->input('id');
         $limit = $request->input('limit', 6);
         $name = $request->input('name');
-        $description = $request->input('description');
-        $tags = $request->input('tags');
-        $categories = $request->input('categories');
-
-        $price_from = $request->input('price_from');
-        $price_to = $request->input('price_to');
+        $show_product = $request->input('show_product');
 
         if($id)
         {
-            $product = Product::with(['category','galleries'])->find($id);
+            $category = ProductCategory::with(['products'])->find($id);
 
-            if($product)
+            if($category)
                 return ResponseFormatter::success(
-                    $product,
+                    $category,
                     'Data produk berhasil diambil'
                 );
             else
                 return ResponseFormatter::error(
                     null,
-                    'Data produk tidak ada',
+                    'Data kategori produk tidak ada',
                     404
                 );
         }
 
-        $product = Product::with(['category','galleries']);
+        $category = ProductCategory::query();
 
         if($name)
-            $product->where('name', 'like', '%' . $name . '%');
+            $category->where('name', 'like', '%' . $name . '%');
 
-        if($description)
-            $product->where('description', 'like', '%' . $description . '%');
-
-        if($tags)
-            $product->where('tags', 'like', '%' . $tags . '%');
-
-        if($price_from)
-            $product->where('price', '>=', $price_from);
-
-        if($price_to)
-            $product->where('price', '<=', $price_to);
-
-        if($categories)
-            $product->where('categories_id', $categories);
+        if($show_product)
+            $category->with('products');
 
         return ResponseFormatter::success(
-            $product->paginate($limit),
-            'Data list produk berhasil diambil'
+            $category->paginate($limit),
+            'Data list kategori produk berhasil diambil'
         );
     }
 }
